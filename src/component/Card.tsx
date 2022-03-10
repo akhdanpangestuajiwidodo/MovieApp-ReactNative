6f59f2cd6c99f10183f4875f73536fecc2444a3c
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-10 11:10:31 +0700

    create card component

---
+++ src/component/Card.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {Image, Text, View, StyleSheet} from 'react-native';
+const Card = () => {
+  return (
+    <View style={stylesCard.card}>
+      <View style={stylesCard.cardContent}>
+        <Image
+          style={stylesCard.cardImage}
+          source={{
+            uri: 'https://lumiere-a.akamaihd.net/v1/images/p_blackwidow_21043_v2_6d1b73b8.jpeg',
+          }}
+        />
+        <View>
+          <Text style={stylesCard.cardTitle}>Black Window</Text>
+          <View style={stylesCard.cardMiddleContainer}>
+            <Text style={stylesCard.cardMiddleText}>Director : Akhdan</Text>
+            <Text style={stylesCard.cardMiddleText}>Duration : 50 min</Text>
+          </View>
+          <Text style={stylesCard.cardRatingText}>8.2</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const stylesCard = StyleSheet.create({
+  card: {
+    backgroundColor: '#1C1C3C',
+    flex: 1,
+    borderRadius: 8,
+  },
+  cardContent: {
+    flexDirection: 'row',
+    padding: 10,
+    alignItems: 'center',
+  },
+  cardImage: {width: 100, height: 100, marginEnd: 12, borderRadius: 4},
+  cardTitle: {color: 'white', fontWeight: 'bold', fontSize: 18},
+  cardMiddleContainer: {marginVertical: 4},
+  cardMiddleText: {color: '#525A62', fontWeight: '700', fontSize: 12},
+  cardRatingText: {color: '#E3C74F', fontWeight: '700', fontSize: 24},
+});
+
+export default Card;
