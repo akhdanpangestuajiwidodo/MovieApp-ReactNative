d69d58894a0da0bf1a7fee029731a22d500e7eb4
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-11 10:20:25 +0700

    change style detail movie

--- src/component/HeaderDetail.tsx
+++ src/component/HeaderDetail.tsx
@@ -1,14 +1,17 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import {Image, StyleSheet, TouchableHighlight, View} from 'react-native';
 
 const HeaderDetail = () => {
   return (
     <View style={stylesHeader.containerHeader}>
-      <Image
-        source={require('../../assets/back.png')}
-        style={stylesHeader.menuHeaderImage}
-      />
+      <TouchableHighlight style={stylesHeader.backButton} underlayColor="#ccc">
+        <Image
+          source={require('../../assets/back.png')}
+          style={stylesHeader.menuHeaderImage}
+        />
+      </TouchableHighlight>
+
       <Image
         source={require('../../assets/favorite.png')}
         style={stylesHeader.favoriteHeaderImage}
@@ -25,6 +28,15 @@ const stylesHeader = StyleSheet.create({
     flexDirection: 'row',
     padding: 10,
     alignItems: 'center',
+  },
+  backButton: {
+    borderRadius: 50 / 2,
+    width: 38,
+    height: 38,
+    backgroundColor: '#322A5Ec0',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingEnd: 2,
   },
   menuHeaderImage: {width: 23, height: 23},
   titleHeader: {color: '#FDFEFF', fontWeight: '700', fontSize: 14},
