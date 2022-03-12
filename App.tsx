55947afc9c5be34997dda981f2bdecbc976ce587
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-12 16:19:01 +0700

    install react navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
 /* eslint-disable prettier/prettier */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import React from 'react';
-import DetailMovie from './src/screen/DetailMovie';
-import FavoriteMovie from './src/screen/FavoriteMovie';
-import Home from './src/screen/Home';
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
-const App = () => {
-  return <Home />;
-};
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
