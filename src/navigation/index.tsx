0393c8892ae776b8177556bb7a0c7f8f068ff089
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-14 07:00:48 +0700

    add store to navigation.tsx

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -5,21 +5,25 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screen/Home';
 import DetailMovie from '../screen/DetailMovie';
 import FavoriteMovie from '../screen/FavoriteMovie';
+import {Provider} from 'react-redux';
+import {Store} from '../store';
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Detail" component={DetailMovie} />
-        <Stack.Screen name="Favorite" component={FavoriteMovie} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={Store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Detail" component={DetailMovie} />
+          <Stack.Screen name="Favorite" component={FavoriteMovie} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
